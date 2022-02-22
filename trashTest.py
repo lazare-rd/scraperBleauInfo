55f74efe3d7584f3deecbb7e1a008f28acfb1f43
lazare-rd <74873908+lazare-rd@users.noreply.github.com>
2022-02-22 09:42:55 +0100

    Delete trashTest.py

--- trashTest.py
+++
@@ -1,9 +0,0 @@
-from decimal import Rounded
-import requests
-import time
-
-t1 = time.time()
-response = requests.get("https://Bleau.info/95.2ouest/304549.html")
-t2 = time.time()
-print(response.elapsed)
-print(round(t2-t1, 4))
